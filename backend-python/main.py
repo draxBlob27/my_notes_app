9c933d61a2b26bee7da4b3340e331448f87bade6
sanil <sparmar27feb2003@gmail.com>
2025-12-16 17:12:06 +0530

    Fix CORS issue

--- backend-python/main.py
+++ backend-python/main.py
@@ -20,7 +20,7 @@ class NoteData(BaseModel):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
